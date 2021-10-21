7147929c593346b6f0ab41db655affd48b707b68
jnieto11 <julionietoduran@gmail.com>
2021-10-21 15:29:55 -0500

    Primer Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=228bpVvfKq1f8Eck0Ck7qJ2JW4hwSIWs`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+
+        }
+    })
+    return gifs;
+}
